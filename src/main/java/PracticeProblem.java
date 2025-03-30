accf5f3b3eab1de15d1201ae0f7b0b254a075c4f
raymond-zeng-ics4u1-03-25 <raymond.zeng4@student.tdsb.on.ca>
2025-03-30 02:33:17 +0000

    update files

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,32 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
-	
-
+	public static int find(int[] array, int number){
+		for (int i = 0; i < array.length;i ++){
+			if (number == array[i]){
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static int findLast(String[] array, String string){
+		for (int i = array.length - 1;i >= 0;i--){
+			if (array[i].equals(string)){
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static int findSecond(char[] array, char character){
+		int x = -1;
+		int count = 0;
+		for (int i = 0;i < array.length; i ++){
+			if (count == 2){break;}
+			if (character == array[i]){
+				count ++;
+				x = i;
+			}
+		}
+		return x;
+	}
 }
